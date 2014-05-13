40899a27475c6dfd69cb990a645287726dd4a6b6
Dave Savage <bedalus@gmail.com>
2014-05-13 11:58:18 -0400

    introduce preservative governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -330,6 +330,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PRESERVATIVE)
+extern struct cpufreq_governor cpufreq_gov_preservative;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_preservative)
 
 #endif
 
