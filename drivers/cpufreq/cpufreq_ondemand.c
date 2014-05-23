76fb8269b560e4df21ff3380bf3d1c5051d93fdd
ihatewebos <mannyb1877@gmail.com>
2014-05-23 13:09:53 -0400

    ondemand: tweak multiphase change table size to 10

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -113,7 +113,7 @@ extern int has_boost_cpu_func;
 #endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_MULTI_PHASE
-#define TABLE_SIZE			14
+#define TABLE_SIZE			10
 #define MAX(x,y)			(x > y ? x : y)
 #define MIN(x,y)			(x < y ? x : y)
 #define FREQ_NEED_BURST(x)	(x < 600000 ? 1 : 0)
