c117e54d17fcca6c1b392daacabaa94d4a051dd3
ihatewebos <mannyb1877@gmail.com>
2014-05-24 07:42:30 -0400

    switch to Flar's ondemand governor

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -36,6 +36,8 @@
 
 extern void set_gpu_clk(unsigned int);
 
+int graphics_boost = 2;
+
 struct clk_pair {
 	const char *name;
 	uint map;
@@ -180,6 +182,9 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 	}
 
 	trace_kgsl_pwrlevel(device, pwr->active_pwrlevel, pwrlevel->gpu_freq);
+
+//graphics_boost
+	graphics_boost = pwr->active_pwrlevel;
 }
 
 EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
