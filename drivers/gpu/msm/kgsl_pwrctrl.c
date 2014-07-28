8552ee7147c2bd2e3a3edcaefe88a2872d8d10eb
ihatewebos <mannyb1877@gmail.com>
2014-07-28 01:37:35 -0400

    drivers: cpu:switch to Flar2's custom ondemand governor

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -34,6 +34,8 @@
 #define UPDATE_BUSY_VAL		1000000
 #define UPDATE_BUSY		50
 
+int graphics_boost = 2;
+
 extern void set_gpu_clk(unsigned int);
 
 struct clk_pair {
@@ -180,6 +182,9 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 	}
 
 	trace_kgsl_pwrlevel(device, pwr->active_pwrlevel, pwrlevel->gpu_freq);
+
+//graphics_boost
+	graphics_boost = pwr->active_pwrlevel;
 }
 
 EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
