83e6634e8253f4b44e6e924c6db1b3395a72e9f6
ihatewebos <mannyb1877@gmail.com>
2014-09-06 21:08:24 -0400

    revert some frequency changes stock frequencies work very well for me.

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -892,7 +892,7 @@ static void __init bus_init(const struct l2_level *l2_level)
 }
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][38];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
 
 static void __init cpufreq_table_init(void)
 {
